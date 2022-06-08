bf6fea054263da1849cd7d771ca9b13ef4edbc87
ShauniArima <shaunisoul@protonmail.com>
2022-06-08 09:13:05 +0000

    feat: Link speakers and sessions

--- 05-project/src/app/sessions.service.ts
+++ 05-project/src/app/sessions.service.ts
@@ -1,36 +1,42 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Session } from './session';
+import { Session, SessionResponse } from './session';
 
-import { catchError, map, mapTo, pluck, retry, toArray } from 'rxjs/operators';
-import { from, throwError } from 'rxjs';
+import { catchError, map, mapTo, mergeMap, pluck, retry, toArray } from 'rxjs/operators';
+import { forkJoin, from, of, throwError } from 'rxjs';
+import { Speaker } from './speaker';
+import { SpeakersService } from './speakers.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionsService {
-  sessions: Array<Session> = [];
-
   API_ROOT = "https://devfest-nantes-2018-api.cleverapps.io/"
 
   constructor(
     private http: HttpClient,
+    private speakersService: SpeakersService,
   ) { }
 
   getSessions() {
-    return this.http.get<Map<string, Session>>(`${this.API_ROOT}sessions`)
-      .pipe(
-        map(Object.values),
-        retry(2), // retry a failed request up to 2 times
-        catchError(this.handleError) // then handle the error
-      );
+    return forkJoin([
+      this.http.get<Map<number, SessionResponse>>(`${this.API_ROOT}sessions`),
+      this.speakersService.getSpeakers(),
+    ]).pipe(
+      map<[Map<number, SessionResponse>, Map<number, Speaker>], [SessionResponse[], Map<number, Speaker>]>(([sessions, speakers]) => [Object.values(sessions), speakers]),
+      map(([sessions, speakers]) => {
+        return sessions.map(session => {
+          return {
+            ...session,
+            speakers: session.speakers?.map(id => speakers[id]),
+          };
+        });
+      }),
+      retry(2), // retry a failed request up to 2 times
+      catchError(this.handleError) // then handle the error
+    );
   }
 
-  clearSessions() {
-    this.sessions = [];
-    return this.sessions;
-  }
-  
   private handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
       console.error('An error occurred:', error.error);
